af253e6ad057f7c7c39a05a3fcb71f149032a3fd
NYCSwan <megan.swanby@gmail.com>
2016-12-25 08:21:14 -0500

    Add example files from DOM chapters

--- duckett/example.js
+++ duckett/example.js
@@ -1,63 +1,63 @@
-// // change 1 element
-// var header = document.getElementById('header');
+// change 1 element
+var header = document.getElementById('header');
 
-// header.className = 'nav';
+header.className = 'nav';
 
 
-// //change nodeList
-// var list = document.querySelectorAll('li[id]');
+//change nodeList
+var list = document.querySelectorAll('li[id]');
 
-// list.className = 'hot';
+list.className = 'hot';
 
-// // change text in an element
-// var itemTwo = document.getElementById('two');
-// var elText = itemTwo.firstChild.nodeValue;
-// elText = elText.replace('nuts', 'kale');
-// itemTwo.firstChild.nodeValue = elText;
+// change text in an element
+var itemTwo = document.getElementById('two');
+var elText = itemTwo.firstChild.nodeValue;
+elText = elText.replace('kale', 'nuts');
+itemTwo.firstChild.nodeValue = elText;
 
-// // Adding content with innerHTML
-// var item = document.getElementById('one')
-// var itemContent = item.innerHTML;
-// item.innerHTML = '<a href=\"http://example.org\">' + itemContent + '</a>';
+// Adding content with innerHTML
+var item = document.getElementById('one')
+var itemContent = item.innerHTML;
+item.innerHTML = '<a href=\"http://example.org\">' + itemContent + '</a>';
 
-// // adding content with DOM manipulation
-// var newItem = document.createElement('li');
-// var newTxt = document.createTextNode('quinoa');
-// newItem.appendChild(newTxt);
-// var position = document.getElementsByTagName('ul')[0];
-// position.appendChild(newItem);
+// adding content with DOM manipulation
+var newItem = document.createElement('li');
+var newTxt = document.createTextNode('quinoa');
+newItem.appendChild(newTxt);
+var position = document.getElementsByTagName('ul')[0];
+position.appendChild(newItem);
 
-// // removing an element
-// var removeEl = document.getElementsByTagName('li')[3];
-// var containerEl = removeEl.parentNode;
-// containerEl.removeChild(removeEl);
+// removing an element
+var removeEl = document.getElementsByTagName('li')[3];
+var containerEl = removeEl.parentNode;
+containerEl.removeChild(removeEl);
 
-// // add items to start and end of list
-// var list2 = document.getElementsByTagName('ul')[0];
+// add items to start and end of list
+var list2 = document.getElementsByTagName('ul')[0];
 
-// var lastNewListItem = document.createElement('li');
-// var lastNewText = document.createTextNode('eggs');
-// lastNewListItem.appendChild(lastNewText);
-// list2.appendChild(lastNewListItem);
+var lastNewListItem = document.createElement('li');
+var lastNewText = document.createTextNode('eggs');
+lastNewListItem.appendChild(lastNewText);
+list2.appendChild(lastNewListItem);
 
-// // add new item to start of list
-// var frontNewListItem = document.createElement('li');
-// var firstNewText = document.createTextNode('bananas');
-// frontNewListItem.appendChild(firstNewText);
-// list2.insertBefore(frontNewListItem, list2.firstChild);
+// add new item to start of list
+var frontNewListItem = document.createElement('li');
+var firstNewText = document.createTextNode('bananas');
+frontNewListItem.appendChild(firstNewText);
+list2.insertBefore(frontNewListItem, list2.firstChild);
 
-// // add a class to all items 
-// var listItems = document.querySelectorAll('li');
-// for(var i = 0; i < listItems.length; i++){
-// 	listItems[i].className ='in-bag';
-// }
+// add a class to all items 
+var listItems = document.querySelectorAll('li');
+for(var i = 0; i < listItems.length; i++){
+	listItems[i].className ='in-bag';
+}
 
 // // add items to the heading
-// var heading = document.querySelector('h2');
-// var headingText = heading.firstChild.nodeValue;
-// var totalItems = listItems.length;
-// var newHeading =  headingText + '<br> Shopping Cart: <span>' + totalItems + '</span>';
-// heading.innerHTML = newHeading;
+var heading = document.querySelector('h2');
+var headingText = heading.firstChild.nodeValue;
+var totalItems = listItems.length;
+var newHeading =  headingText + '<br> Shopping Cart: <span>' + totalItems + '</span>';
+heading.innerHTML = newHeading;
 
 
 // event delegation
@@ -98,8 +98,14 @@ var el = document.getElementById('shoppingList');
 	}
 
 
+// load
 
-
+function setup() {
+	var textInput;
+	textInput = document.getElementById('username');
+	textInput.focus();
+}
+window.addEventListener('load', setup, false);
 
 
 
