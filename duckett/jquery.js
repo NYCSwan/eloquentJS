e945fe491512fe8cb34156028261a833c8a2e297
NYCSwan <megan.swanby@gmail.com>
2016-12-26 15:44:00 -0500

    Add remove and complete functions to list CRUD

--- duckett/jquery.js
+++ duckett/jquery.js
@@ -1,13 +1,15 @@
 $(function() {
 	// setup
-	var $list, $newItemForm, $newItemButton;
+	var $list; 
+	var $newItemForm; 
+	var $newItemButton;
 	var item = '';
 	$list = ('ul');
 	$newItemForm = $('#newItemForm');
 	$newItemButton = $('#newItemButton');
 
 	$('li').hide().each(function(index) {
-		$(this).delay(450 * index).fadeIn(1600);
+		$(this).delay(50 * index).fadeIn(1600);
 	});
 // item counter
 	function updateCount() {
@@ -15,12 +17,39 @@ $(function() {
 		$('#counter').text(items);
 	}
 	updateCount();
-
 // setup form for new items
 	$newItemButton.show();
 	$newItemForm.hide();
 	$('#showForm').on('click', function() {
 		$newItemButton.hide();
 		$newItemForm.show();
+	});
+// add new item to list
+	$newItemForm.on('submit', function(e) {
+		e.preventDefault();
+		var text = $('#itemDescription').val();
+		$('#shoppingList').append("<li>" + text + "</li>");
+		$('input:text').val('');
+		updateCount();
+	});
+// click handling 
+	$('#shoppingList').on('click', 'li', function() {
+		var $this = $(this);
+		var complete = $(this).hasClass('complete');
+		if(complete === true) {
+			$this.animate({
+				opacity: 0.0,
+				left: '+=180'
+			}, 500, 'swing', function() {
+				$this.remove();
+			});
+		} else {
+			item = $this.text();
+			$this.remove();
+			$('#shoppingList')
+				.append('<li class=\"complete\">' + item + '</li>')
+				.hide().fadeIn(300);
+			updateCount();
+		}
 	});
 });
